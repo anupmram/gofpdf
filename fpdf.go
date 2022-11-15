1f83c48c7ecc0916138e81be993629aaf30378e9
Anup Ramakrishnan <110671144+anupmram@users.noreply.github.com>
2022-11-15 17:42:09 +0530

    Update fpdf.go
    //Added ledger size for creating page size compatible with PPT 
    	f.stdPageSizes["ledger"] = SizeType{1224, 792}

--- fpdf.go
+++ fpdf.go
@@ -146,6 +146,8 @@ func fpdfNew(orientationStr, unitStr, sizeStr, fontDirStr string, size SizeType)
 	f.stdPageSizes["letter"] = SizeType{612, 792}
 	f.stdPageSizes["legal"] = SizeType{612, 1008}
 	f.stdPageSizes["tabloid"] = SizeType{792, 1224}
+	//Added ledger size for creating page size compatible with PPT 
+	f.stdPageSizes["ledger"] = SizeType{1224, 792}
 	if size.Wd > 0 && size.Ht > 0 {
 		f.defPageSize = size
 	} else {
